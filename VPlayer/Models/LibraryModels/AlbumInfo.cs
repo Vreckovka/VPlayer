14bb21e0522ae3b61d83b2885260f35229c76ffd
Vreckovka <34897240+Vreckovka@users.noreply.github.com>
2019-03-18 00:15:34 +0100

    Album view

---
+++ VPlayer/Models/LibraryModels/AlbumInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+using VPlayer.LocalMusicDatabase;
+
+namespace VPlayer.Models.LibraryModels
+{
+    [AddINotifyPropertyChangedInterface]
+    class AlbumInfo
+    {
+        public Album Album { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
---
+++ VPlayer/ViewModels/LibraryModels/AlbumsView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VPlayer.LocalMusicDatabase;
+using VPlayer.Models.LibraryModels;
+
+namespace VPlayer.ViewModels.LibraryModels
+{
+    class AlbumsView : BaseLibraryModel
+    {
+        public List<AlbumInfo> Albums { get; set; }
+        private LocalMusicDbContext _localMusicDbContext;
+        public AlbumsView()
+        {
+            _localMusicDbContext = new LocalMusicDbContext();
+
+        }
+
+        public void LoadAlbums(string interpert = null)
+        {
+            if (interpert == null)
+            {
+                Albums = (from x in _localMusicDbContext.Albums
+                    join y in _localMusicDbContext.Artists on x.Artist equals y
+                    select new AlbumInfo()
+                    {
+                        Album = x,
+                        Artist = y
+                    }).ToList();
+
+            }
+        }
+    }
+}
