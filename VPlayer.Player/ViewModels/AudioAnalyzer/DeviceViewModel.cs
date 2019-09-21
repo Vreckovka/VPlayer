92f1dd2e61b1897123c1e89e0107db3112ce44ba
Roman Pecho <roman.pecho@colosseoeas.com>
2019-09-21 17:01:34 +0200

    Player

---
+++ VPlayer.Player/ViewModels/AudioAnalyzer/DeviceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using VCore.ViewModels;
+
+namespace VPlayer.Player.ViewModels.AudioAnalyzer
+{
+  public class DeviceViewModel : ViewModel
+  {
+    public string Name { get; set; }
+    public int BassWasapiIndex { get; set; }
+  }
+}
