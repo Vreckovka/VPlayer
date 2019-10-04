6b8633d0fa1ad6b6ac404cb7ee3097c36fd406b9
Roman Pecho <roman.pecho@colosseoeas.com>
2019-10-04 16:32:51 +0200

    sdf

---
+++ VCore/Converters/VisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VCore.Converters
+{
+  public class VisibilityConverter : BaseConverter
+  {
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value is bool show)
+      {
+        if (show)
+        {
+          return Visibility.Visible;
+        }
+        else
+          return Visibility.Hidden;
+      }
+
+      return Visibility.Collapsed;
+    }
+  }
+}
---
+++ VCore/ItemsCollections/RxObservableCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCore.ItemsCollections
+{
+  public class RxObservableCollection<TItem> : ObservableCollection<TItem>, IDisposable where TItem : class, INotifyPropertyChanged
+  {
+    public Subject<EventPattern<PropertyChangedEventArgs>> ItemUpdated { get; set; }
+    public Subject<EventPattern<TItem>> ItemAdded { get; set; } = new Subject<EventPattern<TItem>>();
+
+    private List<IDisposable> itemsDisposables = new List<IDisposable>();
+    public RxObservableCollection()
+    {
+      CollectionChanged += ObservableItems_CollectionChanged;
+    }
+
+    private void ObservableItems_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+      foreach (var newItem in e.NewItems.OfType<TItem>())
+      {
+        itemsDisposables.Add(Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+          x => newItem.PropertyChanged += x,
+          x => newItem.PropertyChanged -= x).Subscribe(ItemPropertyChanged));
+
+        ItemAdded.OnNext(new EventPattern<TItem>(this,newItem));
+      }
+    }
+
+    private void ItemPropertyChanged(EventPattern<PropertyChangedEventArgs> eventPattern)
+    {
+      ItemUpdated.OnNext(eventPattern);
+    }
+
+    public void Dispose()
+    {
+      ItemUpdated?.Dispose();
+    }
+  }
+}
