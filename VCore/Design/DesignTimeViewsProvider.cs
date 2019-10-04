f397a4b51de5206722f92358c7732c7d43deb0c6
Roman Pecho <roman.pecho@colosseoeas.com>
2019-10-04 14:31:04 +0200

    sfd

---
+++ VCore/Design/DesignTimeViewsProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using VCore.Modularity.Interfaces;
+
+namespace VCore.Design
+{
+  public abstract class DesignTimeViewsProvider
+  {
+    public IEnumerable<IView> GetViewForRegion(string regionName)
+    {
+      return OnGetViewForRegion(regionName);
+    }
+
+    protected abstract IEnumerable<IView> OnGetViewForRegion(string regionName);
+  }
+}
