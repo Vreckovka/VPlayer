99b96695f09942442bc17a899f329b12bccd6963
Roman Pecho <roman.pecho@colosseoeas.com>
2019-10-04 12:20:30 +0200

    dsf

---
+++ VCore/Converters/TimeConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using VCore.Behaviors.Text;
+using VPlayer.Converters;
+
+namespace VCore.Converters
+{
+  public enum TimeType
+  {
+    Second,
+    Minutes,
+    Hours
+  }
+  public class TimeConverter : BaseConverter
+  {
+    #region TimeType
+
+    public TimeType TimeType { get; set; }
+
+    #endregion
+
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value != null)
+      {
+        if (value is TimeSpan)
+        {
+          return null;
+        }
+        else
+        {
+          var time = System.Convert.ToDouble(value);
+
+          switch (TimeType)
+          {
+            case TimeType.Second:
+              return TimeSpan.FromSeconds(time);
+            case TimeType.Minutes:
+              return TimeSpan.FromMinutes(time);
+            case TimeType.Hours:
+              return TimeSpan.FromHours(time);
+            default:
+              throw new ArgumentOutOfRangeException();
+          }
+        }
+      }
+      else
+        return null;
+    }
+  }
+}
