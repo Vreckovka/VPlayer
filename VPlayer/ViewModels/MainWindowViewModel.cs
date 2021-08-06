d13a843c5ddeccd8fb8ccdb90059d8fe6d546dc8
Roman Pecho <f>
2021-08-06 23:52:48 +0200

    vxc

--- VPlayer/ViewModels/MainWindowViewModel.cs
+++ VPlayer/ViewModels/MainWindowViewModel.cs
@@ -35,46 +35,49 @@ namespace VPlayer.ViewModels
 {
 
   //****FEATURES*****
-  //TODO: Cykli ked prejdes cely play list tak ze si ho cely vypocujes (meni sa farba podla cyklu)
-  //TODO: Nacitanie zo suboru
-  //TODO: Ak je neidentifkovana skladba, pridanie interpreta zo zoznamu, alebo vytvorit noveho
-  //TODO: Nastavit si hlavnu zlozku a ked spustis z inej, moznost presunut
-  //TODO: Playlist hore pri menu, quick ze prides a uvidis napriklad 5 poslednych hore v rade , ako carusel (5/5)
-  //TODO: Playlist nech sa automaticky nevytvara ak je niekolko pesniciek (nastavenie pre uzivatela aky pocet sa ma ukladat!) (3/5)
-  //TODO: Hore prenutie medzi windows a browser playermi , zmizne bocne menu
-  //TODO: Pridat loading indikator, mozno aj co prave robi
-  //TODO: Hviezdicky pocet 
-  //TODO: Menu rozdelit na sub menu = Playlists(Songs, Music, TvShows), Library(Albums, Interprets, TvShows), Other(Iptv, UPnP...)
-  //TODO: Popupwindow is TOPMOST
-  //TODO: Reorder na playliste
-  //TODO: Upload lyrics do Google Drive
-  //TODO: Moznost editovat lyrics
-  //TODO: Mozno vyhladat titulky cez search a vybrat si
-  //
-  //
-  //
-  //
-  //
-  //
-  //****TOPKY*****
-  //TODO: Tv show prehrat od posledneho ulozene playlistu (to iste aj pre hudbu kludne), ked pribudne nova seria pusti to kde si skoncil, ale nacita ju
-  //TODO: Vyber output device (zvukove vystupne zariadenie) aj vo fullscreene videa (why the fuck not), miesta do sirky je dost (nie do vysky)
-  //
-  //
-  //
-  //
-  //
-  //
-  //****LONG RUN*****
-  //TODO: Streaming service, aby som nemusel mat db u seba na disku. Nejaky server niekde si kupit (Minio)
-  //TODO: Streaming pre subory, lyrics, tv shows, kludne uplne vsetko (tv sources...). Db tam bude cele to pojde do webu  //
-  //
-  //
-  //
-  //
-  //
-  //****BUGS*****
-  //TODO: Ked pripojim sluchatka cez bluethooth tak spadne appka (ale nie vzdy, pozeral som asi serial)
+        //TODO: Ak je neidentifkovana skladba, pridanie interpreta zo zoznamu, alebo vytvorit noveho
+        //TODO: Nastavit si hlavnu zlozku a ked spustis z inej, moznost presunut
+        //TODO: Playlist hore pri menu, quick ze prides a uvidis napriklad 5 poslednych hore v rade , ako carusel (5/5)
+        //TODO: Playlist nech sa automaticky nevytvara ak je niekolko pesniciek (nastavenie pre uzivatela aky pocet sa ma ukladat!) (3/5)
+        //TODO: Hore prenutie medzi windows a browser playermi , zmizne bocne menu
+        //TODO: Pridat loading indikator, mozno aj co prave robi
+        //TODO: Popupwindow is TOPMOST
+        //TODO: Reorder na playliste
+        //TODO: Upload lyrics do Google Drive
+        //TODO: Moznost editovat lyrics
+        //TODO: Mozno vyhladat titulky cez search a vybrat si
+        //TODO: Hviezdicky pocet 
+        //TODO: Obrazky do listview pri playlistoch (tv show cover atd...)
+        //TODO: TV Channels editor dotiahnut
+        //TODO: Save UPnP Sever (mozno je, bojim sa skusit)
+        //TODO: File Browser - File watcher nad aktualnou zlozkou
+        //TODO: File Browser - Podpora menit cestu
+        //TODO: Popup menu style
+        //TODO: Setting page - Poriadne settingy (ako default cesty do subor napr)
+        //
+        //  ****DESING***** 
+        //      //TODO: Menu rozdelit na sub menu = Playlists(Songs, Music, TvShows), Library(Albums, Interprets, TvShows), Other(Iptv, UPnP...)
+        //      //TODO: Cykli ked prejdes cely play list tak ze si ho cely vypocujes (meni sa farba podla cyklu)
+        //      //TODO: Listview TileView / listview prepinac
+        //      //TODO: TileVIew nad listviewom ked je velky aby bolo iba max poloziek na riadok
+        //
+        //  ****HARD/LONG***** 
+        //      //TODO: Dotiahnut data o albumoch, serialoch (a vyznacit ktore mam a ktore mi chybaju)
+        //
+        //  ****TOPKY*****
+        //      //TODO: Tv show prehrat od posledneho ulozene playlistu (to iste aj pre hudbu kludne), ked pribudne nova seria pusti to kde si skoncil, ale nacita ju
+        //      //TODO: Vyber output device (zvukove vystupne zariadenie) aj vo fullscreene videa (why the fuck not), miesta do sirky je dost (nie do vysky)
+        //
+        //
+        //
+        //****LONG RUN*****
+        //TODO: Streaming service, aby som nemusel mat db u seba na disku. Nejaky server niekde si kupit (Minio)
+        //TODO: Streaming pre subory, lyrics, tv shows, kludne uplne vsetko (tv sources...). Db tam bude cele to pojde do webu  //
+        //
+        //
+        //
+        //****BUGS*****
+        //TODO: Ked pripojim sluchatka cez bluethooth tak spadne appka (ale nie vzdy, pozeral som asi serial)
 
 
 
