03960701629f1e64ad289a62c7920dd460e4b4e0
Roman Pecho <f>
2021-09-07 16:59:49 +0200

    fds

--- VPlayer.AudioStorage/Scrappers/CSFD/ICSFDWebsiteScrapper.cs
+++ VPlayer.AudioStorage/Scrappers/CSFD/ICSFDWebsiteScrapper.cs
@@ -1,14 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using VPlayer.AudioStorage.Scrappers.CSFD.Domain;
 
 namespace VPlayer.AudioStorage.Scrappers.CSFD
 {
   public interface ICSFDWebsiteScrapper
   {
-    CSFDTVShow LoadTvShow(string url, int? seasonNumber = null, int? episodeNumber = null);
+    CSFDTVShow LoadTvShow(string url,CancellationToken cancellationToken, int? seasonNumber = null, int? episodeNumber = null);
 
-    CSFDTVShowSeason LoadTvShowSeason(string url);
-    Task<CSFDQueryResult> FindItems(string name);
-    Task<CSFDItem> GetBestFind(string name, int? year = null, bool onlySingleItem = false, string tvShowUrl = null, string tvShowName = null, int? seasonNumber = null, int? episodeNumber = null);
+    CSFDTVShowSeason LoadTvShowSeason(string url, CancellationToken cancellationToken);
+    Task<CSFDQueryResult> FindItems(string name, CancellationToken cancellationToken);
+    Task<CSFDItem> GetBestFind(
+      string name,
+      CancellationToken cancellationToken,
+      int? year = null, 
+      bool onlySingleItem = false, 
+      string tvShowUrl = null,
+      string tvShowName = null,
+      int? seasonNumber = null, 
+      int? episodeNumber = null);
   }
 }
