c6aefee044640323f0dd4137ebe4af3b78033152
Roman Pecho <f>
2021-12-21 19:31:14 +0100

    dsa

--- VPlayer.Core/FileBrowser/PlayableFileViewModel.cs
+++ VPlayer.Core/FileBrowser/PlayableFileViewModel.cs
@@ -56,6 +56,8 @@ namespace VPlayer.Core.FileBrowser
       }
     }
 
+    #region Properties
+
     public ObservableCollection<ThumbnailViewModel> Thumbnails { get; } = new ObservableCollection<ThumbnailViewModel>();
 
     #region ThumbnailsLoading
@@ -96,6 +98,8 @@ namespace VPlayer.Core.FileBrowser
 
     #endregion
 
+    #endregion
+
     #region Commands
 
     #region PlayButton
@@ -117,7 +121,116 @@ namespace VPlayer.Core.FileBrowser
 
     #endregion
 
+    #region CreateThumbnails
+
+    private ActionCommand createThumbnails;
+
+    public ICommand CreateThumbnails
+    {
+      get
+      {
+        if (createThumbnails == null)
+        {
+          createThumbnails = new ActionCommand(async () => await OnCreateThumbnails());
+        }
+
+        return createThumbnails;
+      }
+    }
+
+    #region OnCreateThumbnails
+
+    private async Task OnCreateThumbnails()
+    {
+      if (Thumbnails.Count == 0)
+      {
+        try
+        {
+          ThumbnailsLoading = true;
+          Thumbnails.Clear();
+          var thmbs = new List<ThumbnailViewModel>();
+
+          await Task.Run(async () =>
+          {
+            var ffprobeResult = await iVFfmpegProvider.RunFfprobe<ProcessOutputHandler>($"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{Model.FullName}\"");
+
+            var allOutput = ffprobeResult.Output.ToList();
+            double? fileDuration = null;
+            int numberOfScreenshots = 5;
+
+            var thumbsFolder = "preview_thumbnails";
+
+            Directory.CreateDirectory(thumbsFolder);
+
+            var dur = allOutput.FirstOrDefault(x => x != null);
+
+            if (double.TryParse(dur, out var parsed))
+            {
+              fileDuration = parsed * 0.9;
+            }
+
+            if (fileDuration != null)
+            {
+              int screenInterval = (int)fileDuration / numberOfScreenshots;
+              string ffmpegParams = " ";
+
+              for (int i = 0; i < numberOfScreenshots; i++)
+              {
+                var actualDuration = (i + 1) * screenInterval;
+
+                var time = TimeSpan.FromSeconds(actualDuration);
+                var timeStr = TimeSpan.FromSeconds(actualDuration).ToString(@"hh\:mm\:ss");
+
+                ffmpegParams += $"-ss {timeStr} -i \"{Model.FullName}\" ";
+
+                thmbs.Add(new ThumbnailViewModel()
+                {
+                  Time = time
+                });
+              }
+
+              for (int i = 0; i < numberOfScreenshots; i++)
+              {
+                ffmpegParams += $"-map {i}:v -vframes 1 {thumbsFolder}\\output_{i + 1}.png ";
+              }
+
+              ffmpegParams = ffmpegParams + " -y";
+
+              var result = await iVFfmpegProvider.RunFfmpeg<ProcessOutputHandler>(ffmpegParams, true);
+
+              for (int i = 0; i < numberOfScreenshots; i++)
+              {
+                var file = $"{thumbsFolder}\\output_{i + 1}.png";
+
+                if (File.Exists(file))
+                {
+                  var bytes = File.ReadAllBytes(file);
+
+                  thmbs[i].ImageData = bytes;
+
+                  File.Delete(file);
+                }
+              }
+            }
+          });
+
+          Thumbnails.AddRange(thmbs);
+        }
+        finally
+
+        {
+          ThumbnailsLoading = false;
+        }
+      }
+    }
+
     #endregion
+
+    #endregion
+
+    #endregion
+
+    #region Methods
 
     #region Play
 
@@ -162,106 +275,6 @@ namespace VPlayer.Core.FileBrowser
 
     #endregion
 
-    #region OnSelected
-
-    protected override async void OnSelected(bool isSelected)
-    {
-      base.OnSelected(isSelected);
-
-      if (isSelected)
-      {
-        await CreateThumbnails();
-      }
-
-      base.OnSelected(isSelected);
-    }
-
-    #endregion
-
-    #region CreateThumbnails
-
-    private async Task CreateThumbnails()
-    {
-      try
-      {
-        ThumbnailsLoading = true;
-        Thumbnails.Clear();
-        var thmbs = new List<ThumbnailViewModel>();
-
-        await Task.Run(async () =>
-        {
-          var ffprobeResult = await iVFfmpegProvider.RunFfprobe<ProcessOutputHandler>($"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{Model.FullName}\"");
-
-          var allOutput = ffprobeResult.Output.ToList();
-          double? fileDuration = null;
-          int numberOfScreenshots = 5;
-
-          var thumbsFolder = "preview_thumbnails";
-
-          Directory.CreateDirectory(thumbsFolder);
-
-          var dur = allOutput.FirstOrDefault(x => x != null);
-
-          if (double.TryParse(dur, out var parsed))
-          {
-            fileDuration = parsed * 0.9;
-          }
-
-          if (fileDuration != null)
-          {
-            int screenInterval = (int)fileDuration / numberOfScreenshots;
-            string ffmpegParams = " ";
-
-            for (int i = 0; i < numberOfScreenshots; i++)
-            {
-              var actualDuration = (i + 1) * screenInterval;
-
-              var time = TimeSpan.FromSeconds(actualDuration);
-              var timeStr = TimeSpan.FromSeconds(actualDuration).ToString(@"hh\:mm\:ss");
-
-              ffmpegParams += $"-ss {timeStr} -i \"{Model.FullName}\" ";
-
-              thmbs.Add(new ThumbnailViewModel()
-              {
-                Time = time
-              });
-            }
-
-            for (int i = 0; i < numberOfScreenshots; i++)
-            {
-              ffmpegParams += $"-map {i}:v -vframes 1 {thumbsFolder}\\output_{i + 1}.png ";
-            }
-
-            ffmpegParams = ffmpegParams + " -y";
-
-            var result = await iVFfmpegProvider.RunFfmpeg<ProcessOutputHandler>(ffmpegParams, true);
-
-            for (int i = 0; i < numberOfScreenshots; i++)
-            {
-              var file = $"{thumbsFolder}\\output_{i + 1}.png";
-
-              if (File.Exists(file))
-              {
-                var bytes = File.ReadAllBytes(file);
-
-                thmbs[i].ImageData = bytes;
-
-                File.Delete(file);
-              }
-            }
-          }
-        });
-
-        Thumbnails.AddRange(thmbs);
-      }
-      finally
-      {
-        ThumbnailsLoading = false;
-      }
-    }
-
-    #endregion
-
     #region OnOpenContainingFolder
 
     public override void OnOpenContainingFolder()
@@ -289,5 +302,6 @@ namespace VPlayer.Core.FileBrowser
 
     #endregion
 
+    #endregion
   }
 }
